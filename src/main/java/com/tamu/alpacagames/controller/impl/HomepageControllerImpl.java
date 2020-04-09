d752d82cce3ae5764748940e9bce030760a3b5f0
Chetan <Chetan@DESKTOP-EGSKCLS>
2020-04-08 19:43:46 -0500

    Updating controller 4

--- src/main/java/com/tamu/alpacagames/controller/impl/HomepageControllerImpl.java
+++ src/main/java/com/tamu/alpacagames/controller/impl/HomepageControllerImpl.java
@@ -1,19 +1,15 @@
 package com.tamu.alpacagames.controller.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tamu.alpacagames.controller.HomepageController;
-import com.tamu.alpacagames.model.Game;
+import com.tamu.alpacagames.model.Users;
 import com.tamu.alpacagames.service.GameService;
 
 @Controller
@@ -28,10 +24,8 @@ public class HomepageControllerImpl implements HomepageController {
 	 * @GetMapping("/") public List<Game> getFrontGames() { List<Game>
 	 * homepageGames = gameService.getHomepageGames(); return homepageGames; }
 	 */
-	
-	
 	@Override
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView getFrontGames(Model model) {
 		model.addAttribute("games", gameService.getHomepageGames());
 		//List<Game> homepageGames = gameService.getHomepageGames();
