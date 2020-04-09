3ddc76660827d648d113b1627cd3b3c59ced1c8e
Chetan <Chetan@DESKTOP-EGSKCLS>
2020-04-08 19:51:48 -0500

    test 8

--- src/main/java/com/tamu/alpacagames/controller/impl/HomepageControllerImpl.java
+++ src/main/java/com/tamu/alpacagames/controller/impl/HomepageControllerImpl.java
@@ -1,18 +1,21 @@
 package com.tamu.alpacagames.controller.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tamu.alpacagames.controller.HomepageController;
-import com.tamu.alpacagames.model.Users;
+import com.tamu.alpacagames.model.Game;
 import com.tamu.alpacagames.service.GameService;
 
-@Controller
+@RestController
 public class HomepageControllerImpl implements HomepageController {
 	
 	@Autowired
@@ -25,17 +28,13 @@ public class HomepageControllerImpl implements HomepageController {
 	 * homepageGames = gameService.getHomepageGames(); return homepageGames; }
 	 */
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView getFrontGames() {
-		//model.addAttribute("games", gameService.getHomepageGames());
+	
+	@Override
+	@GetMapping("/")
+	public ModelAndView getFrontGames(Model model) {
+		model.addAttribute("games", gameService.getHomepageGames());
 		//List<Game> homepageGames = gameService.getHomepageGames();
 		return new ModelAndView("html/index");
-	}
-
-	@Override
-	public ModelAndView getFrontGames(Model model) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 	
 	
